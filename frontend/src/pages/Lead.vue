f95c62c9d93eab576ea12bbf449f48b408d9b2e5
Islam Hassan <islam.abuaish@gmail.com>
2026-01-21 19:18:59 +0300

    edit text only

--- frontend/src/pages/Lead.vue
+++ frontend/src/pages/Lead.vue
@@ -420,7 +420,7 @@ const tabs = computed(() => {
     },
     {
       name: 'Attachments',
-      label: __('Attachments'),
+      label: __('AttachmentsOrContracts'),
       icon: AttachmentIcon,
     },
     {
