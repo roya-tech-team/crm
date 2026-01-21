c12e3aa785b1feca412f2c07e5e6584fbb4439a6
Islam Hassan <islam.abuaish@gmail.com>
2026-01-21 20:05:38 +0300

    fix: simplify label for Attachments tab by removing 'or Contracts'

--- frontend/src/components/Activities/Activities.vue
+++ frontend/src/components/Activities/Activities.vue
@@ -710,7 +710,7 @@ const emptyText = computed(() => {
   } else if (title.value == 'Tasks') {
     text = 'No Tasks'
   } else if (title.value == 'Attachments') {
-    text = 'No Attachments or Contracts'
+    text = 'No Attachments'
   } else if (title.value == 'WhatsApp') {
     text = 'No WhatsApp Messages'
   }
